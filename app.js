bf27cd833cb46a9a5c58fb9013bc0d4dc8026769
Eduardo Tomita <tomita@edu.univali.br>
2017-05-31 17:36:59 -0300

    Triangular e Uniforme

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var amostras_nao_repetidas = [];
 for (var i = 0; i < qtd_amostas; i++){
     amostras[i] = Math.floor((Math.random() * 10) +1) ;
 }
-console.log("Amostras: "+amostras);
+console.log("Amostras: "+ amostras);
 
 for(var i = 0; i < amostras.length; i++){		
     amostras_nao_repetidas[i] = amostras[i];				
@@ -24,22 +24,80 @@ for (var i = 0; i < amostras_nao_repetidas.length; i++) {
         }
     }							  
 }
+console.log("Amostras não repetidas: "+ amostras_nao_repetidas);
+//poisson();
+/*Poisson */
+function poisson () {
+    for (var i = 0; i < amostras_nao_repetidas.length; i++){
+        var num = amostras_nao_repetidas[i];            
+        var qtd = quantas_vezes_aparece(num);
+        lambda = qtd/qtd_amostas;    
+        console.log('\nNúmero '+ num + ' apareceu '+ qtd +' vezes ');
+        console.log('Lambda = '+ lambda +'\nPoisson('+ num +') = '+dist.poisson(qtd, lambda));    
+    }
+}
 
-//console.log("Números sorteados(repetidos): "+amostras_nao_repetidas);
-for (var i = 0; i < amostras_nao_repetidas.length; i++){
-    //console.log (amostras_nao_repetidas[i]+' apareceu: '+quantas_vezes_aparece(amostras_nao_repetidas[i])+" vezes");        
-    var qtd = quantas_vezes_aparece(amostras_nao_repetidas[i]);
-    lambda = qtd/qtd_amostas;
-    console.log(amostras_nao_repetidas[i] + ' => '+qtd+' ---> '+lambda+' => '+dist.poisson(qtd, lambda));
+/*Triangular */
+//triangular();
+function triangular (){
+    var mais_repete = numero_mais_repete();
+    var maior = maior_encontrado();
+    var menor = menor_encontrado(); 
+    console.log("Maior: "+maior);
+    console.log("Menor: "+menor);
+    console.log("Mais repete: "+mais_repete);    
+    for (var i = 0; i < amostras_nao_repetidas.length; i++){    
+        var num = amostras_nao_repetidas[i];     
+        var qtd = quantas_vezes_aparece(num);                     
+        console.log('\nNúmero '+ num + ' apareceu '+ qtd +' vezes ');
+        console.log('Triangular('+ num +') = '+dist.triangular(maior, menor, mais_repete, num));    
+    }    
+}
+
+/*Uniforme */
+uniforme();
+function uniforme (){
+    var maior = maior_encontrado();
+    var menor = menor_encontrado(); 
+    console.log("Maior: "+maior);
+    console.log("Menor: "+menor);   
+    for (var i = 0; i < amostras_nao_repetidas.length; i++){    
+        var num = amostras_nao_repetidas[i];     
+        var qtd = quantas_vezes_aparece(num);                     
+        console.log('\nNúmero '+ num + ' apareceu '+ qtd +' vezes ');
+        console.log('Uniforme('+ num +') = '+dist.uniforme(maior, menor, num));    
+    }      
 }
 
 function quantas_vezes_aparece (num){
     var cont = 0;
-    for(var i =0; i< amostras.length; i++){
+    for(var i = 0; i< amostras.length; i++){
         if(amostras[i] == num){
             cont++;
         }
     }
     return cont;
+}
+
+function maior_encontrado (){
+    return Math.max.apply(null, amostras_nao_repetidas)
+}
+
+function menor_encontrado (){
+    return Math.min.apply(null, amostras_nao_repetidas)
+}
+
+function numero_mais_repete (){
+    var cont_inicial = 0;
+    var cont_atual = 0;
+    var num = 0;
+    for (var i = 0; i < amostras_nao_repetidas.length; i++){
+        cont_atual = quantas_vezes_aparece(amostras_nao_repetidas[i]);
+        if (cont_atual >= cont_inicial){
+            num = amostras_nao_repetidas[i];
+            cont_inicial = cont_atual;
+        }        
+    }       
+    return num; 
 }
 
--- distributions.js
+++ distributions.js
@@ -3,6 +3,7 @@ var factorial = require( 'math-factorial' );
 
 var pow = Math.pow;
 const logNatural = 2.71;
+
 module.exports = {
     /**
      *
@@ -17,5 +18,47 @@ module.exports = {
 
         return toReturn
     },
+
+    /**
+     *
+     * @param maior Maior número sorteado.
+     * @param menor Menor número sorteado.
+     * @param mais Número que mais se repete
+     * @param n Número dentro das amostras     
+     * @returns {number} Resultado triangular
+     */
+    triangular: function(maior, menor, mais, n) {                            
+        var toReturn = null;
+        if(n >= menor && n < mais){
+            toReturn = (2*(n - menor)) / ((maior - menor) * (mais - menor));
+        }else if(n == mais){
+            toReturn = 2/(maior - menor);
+        }else if(n > mais && n <= maior){
+            toReturn = (2*(maior - n)) / ((maior - menor) * (maior - mais));
+        }else{
+            toReturn = 0;
+        }        
+
+        return toReturn
+    },   
+
+    /**
+     *
+     * @param maior Maior número sorteado.
+     * @param menor Menor número sorteado.
+     * @param n Número dentro das amostras     
+     * @returns {number} Resultado triangular
+     */
+    uniforme: function(maior, menor, n) {                            
+        var toReturn = null;
+
+        if(n >= menor && n <= maior){
+            toReturn = 1/(maior - menor);
+        }else{
+            toReturn = 0;
+        }   
+
+        return toReturn
+    },      
 
 };
